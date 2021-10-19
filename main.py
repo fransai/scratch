8272fa568aa823cf1ad35fb4479d621f70fd38ec
Fran <38441102+fransai@users.noreply.github.com>
2021-10-19 19:57:43 +0000

    Added a example of how to use urllib.request library rather than socket

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import socket
+import urllib.request as request
+
+#Using Socket Lib
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysocket.send(cmd)
 
 while True:
@@ -12,4 +15,8 @@ while True:
 	print(response.decode())
 mysocket.close()
 
+#Using urllib.request lib
 
+response = request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in response:
+	print(line.decode().strip())
