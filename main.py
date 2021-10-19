29882293c2c3eefd0eb65b21e78de0802b6cae3e
Fran <38441102+fransai@users.noreply.github.com>
2021-10-19 19:09:43 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysocket.send(cmd)
+
+while True:
+	response = mysocket.recv(512)
+	if (len(response) < 1):
+		break
+	print(response.decode())
+mysocket.close()
+
+
